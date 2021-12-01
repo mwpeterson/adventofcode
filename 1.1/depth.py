af398a782244fd1f72440012e56e0dc33ba0b3da
mwpeterson <michael@mwpeterson.com>
2021-12-01 16:30:38 -0500

    day1

---
+++ 1.1/depth.py
@@ -0,0 +1,31 @@
+
+def read_input(filename):
+    with open(filename) as f:
+        data = f.read().splitlines()
+    return list(map(int, data))
+
+
+def count_increase(lst, window):
+    """
+    Return the number of times the list is increasing.
+    """
+    count = 0
+    for i in range(len(lst) - window):
+        a = sum(lst[i:i+window])
+        b = sum(lst[i+1:i+1+window])
+        if a < b:
+             count += 1
+    return count
+
+
+def main():
+    """
+    Read input from file and count the number of times the list is increasing.
+    """
+    data = read_input("input.txt")
+    print(count_increase(data, 1))
+    print(count_increase(data, 3))
+
+
+if __name__ == "__main__":
+    main()
