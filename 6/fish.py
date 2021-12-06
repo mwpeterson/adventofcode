896df5c8ce28dd585365b2c1f1e4c42a577b2b78
mwpeterson <michael@mwpeterson.com>
2021-12-06 14:08:37 -0500

    add timing

--- 6/fish.py
+++ 6/fish.py
@@ -1,4 +1,6 @@
 import numpy as np
+from timeit import default_timer as timer
+from datetime import timedelta
 
 
 def spawn(bins, days):
@@ -10,11 +12,22 @@ def spawn(bins, days):
 
 
 def main():
+    start = timer()
     data = np.loadtxt('input.txt', delimiter=',', unpack=True, dtype=np.int64)
+    load = timer()
     bins = np.bincount(data, minlength=9)
+    bin = timer()
     print(spawn(bins, 80))
+    lap = timer()
     print(spawn(bins, 256))
+    end = timer()
 
+    print("loadtxt: {} ms".format(timedelta(seconds=load - start).total_seconds() *1000))
+    print("bin: {} μs".format(timedelta(seconds=bin - load).total_seconds() * 1000*1000))
+    print("80 days: {} μs".format(timedelta(seconds=lap - bin).total_seconds() * 1000*1000))
+    print("256 days: {} μs".format(timedelta(seconds=end - lap).total_seconds() * 1000*1000))
+    print("total compute: {} μs".format(timedelta(seconds=end - load).total_seconds() * 1000*1000))
+    print("overall: {} ms".format(timedelta(seconds=end - start).total_seconds() *1000))
 
 if __name__ == '__main__':
     main()
