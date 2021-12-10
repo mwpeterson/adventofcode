9e939821133b8db556199eca12f7be6019f8bca5
mwpeterson <michael@mwpeterson.com>
2021-12-10 15:20:42 -0500

    remove deque and exception logic

--- 10/syntax.py
+++ 10/syntax.py
@@ -1,5 +1,3 @@
-from collections import deque
-from statistics import median
 from timeit import default_timer as timer
 from datetime import timedelta
 
@@ -10,45 +8,31 @@ def check_syntax(nav):
     closer = set([')', ']', '}', '>'])
     match = {'(': ')', '[': ']', '{': '}', '<': '>'}
     scoring = {'(': 1, '[': 2, '{': 3, '<': 4, ')': 3, ']': 57, '}': 1197, '>': 25137}
-    while True:
-        try:
-            line = nav.pop()
-            syntax = deque()
-
-            while True:
-                try:
-                    c = line.popleft()
-                    if c in opener:
-                        syntax.append(c)
-                    elif c in closer:
-                        d = syntax.pop()
-                        if c != match[d]:
-                            raise ValueError(c)
-                except ValueError as e:
-                    score = score + scoring[e.args[0]]
+    for line in nav:
+        syntax = []
+        for c in line:
+            if c in opener:
+                syntax.append(c)
+            elif c in closer:
+                d = syntax.pop()
+                if c != match[d]:
+                    score = score + scoring[c]
                     break
-                except IndexError:
-                    complete = 0
-                    while True:
-                        try:
-                            m = syntax.pop()
-                            complete = 5*complete + scoring[m]
-                        except IndexError:
-                            auto.append(complete)
-                            break
-                    break
-        except IndexError:
-            break
-
-    return (score, median(sorted(auto)))
+        else:
+            complete = 0
+            for m in syntax[::-1]:
+                complete = 5*complete + scoring[m]
+            auto.append(complete)
+            continue
+    return (score, sorted(auto)[len(auto)//2])
 
 
 def main():
     start = timer()
-    nav = deque()
+    nav = []
     with open('input') as f:
         for line in f:
-            nav.append(deque(line.rstrip('\n')))
+            nav.append(line.rstrip('\n'))
     load = timer()
     print(check_syntax(nav))
     end = timer()
