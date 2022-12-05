467be386569dc379b9b9d53f2d516bb1030bc22d
mwpeterson <michael@mwpeterson.com>
2022-12-05 13:28:54 -0500

    part 2

--- 05/crates.sh
+++ 05/crates.sh
@@ -2,45 +2,42 @@
 
 shopt -s extglob                     # turn on extended glob
 
-file=$1
+split -p "^$" -a 1 $1
 
-split -p "^$" -a 1 $file
+read -r count < <(tail -1 xa)
+max=${count##* }
 
-part1=""
-
-max=0
-stack=()
-while read -r count; do
-  for c in $count; do
-    max=$c
-  done
-done < <(tail -1 xa)
-echo $max
-
+stack1=()
 while IFS= read -r start; do
   for (( c=1; c<=$max; c++ )); do
     i=$(( ( c - 1 ) * 4 + 1 ))
-    stack[$c]+="${start:$i:1}"
+    stack1[$c]+="${start:$i:1}"
   done
 done < <(tail -r xa | tail -n +2)
 
-declare -p stack
-stack=("" "${stack[@]/%+([[:blank:]])/}" ) # remove trailing space/tab from each element
-declare -p stack
+stack1=("" "${stack1[@]/%+([[:blank:]])/}" ) # remove trailing space/tab from each element
+
+stack2=("${stack1[@]}")
 
 while read -r line; do
   read junk m junk f junk t <<< "$line"
+  # part 1 with CrateMover 9000
   for (( i=1; i<=$m; i++ )); do
-    from=${stack[$f]}
-    to=${stack[$t]}
-    stack[$t]+=${from:(-1)}
-    stack[$f]=${from%?}
+    from=${stack1[$f]}
+    stack1[$t]+=${from: -1}
+    stack1[$f]=${from%?}
   done
+  # part 2 with CrateMover 9001
+  from=${stack2[$f]}
+  stack2[$t]+=${from: -$m}
+  stack2[$f]=${from:0:${#from}-$m}
 done < <(tail -n +2 xb)
-declare -p stack
 
 for (( c=1; c<=$max; c++ )); do
-  part1="$part1${stack[$c]:(-1)}"
+  part1+=${stack1[$c]: -1}
+  part2+=${stack2[$c]: -1}
 done
 echo $part1
+echo $part2
+
 rm xa xb
